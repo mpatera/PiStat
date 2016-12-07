6035e4cc40a1c8a05c3d3016b3bb19856172e074
Darren <darren.enos@umconnect.umt.edu>
2016-12-06 20:17:01 -0700

    Temperature object added to environment

--- public/js/environment.js
+++ public/js/environment.js
@@ -5,6 +5,43 @@
 
 //create temp object
     //attach sensor 'observers'
+function temperature() {
+    val: 55,
+    this.handlers = []; // observer
 
+}
+
+temperature.prototype = {
+
+    subscribe: function(fn) {
+        this.handlers.push(fn);
+    },
+
+    unsubscribe: function(fn) {
+        this.handlers = this.handlers.filter(
+            function(item) {
+                if (item !== fn){
+                    return item;
+                }
+            }
+        );
+    },
+
+    increase: function(o, thisObj) {
+        this.val += 1;
+        var scope = thisObj || window;
+        this.handlers.forEach(function(item){
+            item.call(scope, o);
+        });
+    },
+
+    decrease: function(o, thisObj) {
+        this.val -= 1;
+        var scope = thisObj || window;
+        this.handlers.forEach(function(item){
+            item.call(scope, o);
+        });
+    }
+};
 //create door object
     //attach sensor 'observers'
--- public/js/sensor.js
+++ public/js/sensor.js
@@ -1,25 +1,7 @@
 /**
  * Created by darren on 12/6/16.
  */
-var sensor = {
-    cur_val: 55,
-    open: false,
-    change: function(dir,type) {
-        if (type == 0){ //temperature
-            if (dir > 0) {
-                cur_val += 1;
-            }
-            else {
-                cur_val -= 1;
-            }
-        }
-        else{       //door
-            if(dir == 1){
-                open = true;
-            }
-            else{
-                open = false;
-            }
-        }
-    }
+var sensor = function(tmp) {
+    console.log('temp changed to '+tmp);
+    //send tmp to current temp dial in index.html
 }
