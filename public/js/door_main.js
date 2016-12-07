42822bb89c8972189845abeb49c44d07aa290aa8
mpatera <mattpatera1994@gmail.com>
2016-12-07 05:53:08 -0700

    Commented door_main

--- public/js/door_main.js
+++ public/js/door_main.js
@@ -1,33 +1,43 @@
 /**
  * Created by mattpatera on 12/7/16.
+ *
+ * Contains logic for the function of the door alarm service
  */
 
 var door = (function () {
+
+    // PRIVATE VARIABLES
     var isOpen = false;
 
+    // PRIVATE FUNCTIONS
     function makeRequest(attempt = 1) {
+        // chain of command begins
         if (attempt == 1) {
             var getURL = 'http://localhost:3000/alertSMS';
             var service = 'SMS';
         } else if (attempt == 2) {
             var getURL = 'http://localhost:3000/alertEmail';
             var service = 'Email';
+        } else {
+            console.log("You're in some trouble dawg.");
         }
 
         $.get(getURL, '', function () {
             console.log(service + " sent")
         }).fail(function () {
             console.log("Request " + attempt + " has failed.");
-            makeRequest(attempt + 1);
+            makeRequest(attempt + 1); // on fail, increment chain id
         });
     }
 
     function act() {
+        // if door is open and system is armed
         if (isOpen && sec_system.isSystemArmed()) {
             makeRequest();
         }
     }
 
+    // PUBLIC FUNCTIONS
     return {
         door_mod: function () {
             isOpen = !isOpen;
@@ -41,8 +51,10 @@ var door = (function () {
 })();
 
 var sec_system = (function () {
+    // PRIVATE VARIABLES
     var isArmed = false;
 
+    // PRIVATE FUNCTIONS
     function changeIcon() {
 
         var element = $('#status-icon');
@@ -54,6 +66,7 @@ var sec_system = (function () {
         }
     }
 
+    // PUBLIC FUNCTIONS
     return {
         system_mod: function () {
             isArmed = !isArmed;
@@ -66,6 +79,7 @@ var sec_system = (function () {
     }
 })();
 
+// JQUERY TO ACT ON SINGLETONS ABOVE
 $(function ($) {
     $('#open_close_door').click(function () {
         door.door_mod();
